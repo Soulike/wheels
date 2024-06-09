7067fdb32e27d5e729db4d3dd3523e0abebb80fd
Soulike <git_anonymous@foxmail.com>
2024-06-09 12:52:22 +0800

    feat: revamp cpp union_find_set

--- UnionFindSet/union_find_set.cpp
+++ UnionFindSet/union_find_set.cpp
@@ -1,47 +1,41 @@
-class union_find_set {
- public:
-  using element_type = unsigned long long;
-  using size_type = unsigned long long;
+#include "union_find_set.h"
 
- private:
-  size_type _set_count;
-  const size_type _size;
-  element_type* const parent;
-
- public:
-  union_find_set(size_type n) : _size(n), parent(new element_type[n]) {
-    for (int i = 0; i < n; i++) {
-      parent[i] = i;
-    }
-    _set_count = n;
+union_find_set::union_find_set(union_find_set::size_type n) {
+  _parents.resize(n);
+  for (int i = 0; i < n; i++) {
+    _parents[i] = i;
   }
+  _set_count = n;
+}
 
-  ~union_find_set() { delete[] parent; }
+union_find_set::size_type union_find_set::size() const noexcept {
+  return _parents.size();
+}
 
-  size_type size() const { return _size; }
+union_find_set::size_type union_find_set::set_count() noexcept {
+  return _set_count;
+}
 
-  size_type set_count() const { return _set_count; }
-
-  void do_union(element_type element1, element_type element2) {
-    element_type aSet = find(element1);
-    element_type bSet = find(element2);
-    if (aSet != bSet) {
-      _set_count--;
-      parent[bSet] = aSet;
-    }
+void union_find_set::do_union(const element_type& element1,
+                              const element_type& element2) {
+  element_type set_a = find(element1);
+  element_type set_b = find(element2);
+  if (set_a != set_b) {
+    _set_count--;
+    _parents[set_b] = set_a;
   }
+}
 
-  bool isInOneSet(element_type element1, element_type element2) {
-    return find(element1) == find(element2);
-  }
+bool union_find_set::is_in_one_set(const element_type& element1,
+                                   const element_type& element2) {
+  return find(element1) == find(element2);
+}
 
- private:
-  element_type find(element_type element) {
-    element_type currentMember = element;
-    while (parent[currentMember] != currentMember) {
-      currentMember = parent[currentMember];
-      parent[currentMember] = find(parent[currentMember]);
-    }
-    return currentMember;
+union_find_set::element_type union_find_set::find(const element_type& element) {
+  element_type current_element = element;
+  while (_parents[current_element] != current_element) {
+    current_element = _parents[current_element];
+    _parents[current_element] = find(_parents[current_element]);
   }
-};
+  return current_element;
+}
---
+++ UnionFindSet/union_find_set.h
@@ -0,0 +1,25 @@
+#ifndef WHEELS_UNION_FIND_SET_H
+#define WHEELS_UNION_FIND_SET_H
+#include <vector>
+
+class union_find_set {
+ public:
+  using element_type = unsigned long long;
+  using size_type = std::vector<element_type>::size_type;
+
+  explicit union_find_set(size_type n);
+  ~union_find_set() = default;
+  size_type size() const noexcept;
+  size_type set_count() noexcept;
+  void do_union(const element_type& element1, const element_type& element2);
+  bool is_in_one_set(const element_type& element1,
+                     const element_type& element2);
+
+ private:
+  size_type _set_count;
+  std::vector<size_type> _parents;
+
+  element_type find(const element_type& element);
+};
+
+#endif  // WHEELS_UNION_FIND_SET_H
