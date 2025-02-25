590904151e07eda15c37eafdcaeee02d4f72bf36
Soulike <git_anonymous@foxmail.com>
2025-02-26 07:25:52 +0800

    feat(Math): optimize combination function for better performance

--- Math/combination.cpp
+++ Math/combination.cpp
@@ -1,9 +1,12 @@
 #include "combination.h"
 
-std::uintmax_t combination(const std::uintmax_t totalNumber,
-                           const std::uintmax_t selectionNumber) {
+#include <algorithm>
+
+std::uintmax_t combination(std::uintmax_t totalNumber,
+                           std::uintmax_t selectionNumber) {
   std::uintmax_t result = 1;
-  for (std::uintmax_t i = 1; i <= (totalNumber - selectionNumber); i++) {
+  selectionNumber = std::max(selectionNumber, totalNumber - selectionNumber);
+  for (std::uintmax_t i = 1; i <= totalNumber - selectionNumber; i++) {
     result *= (selectionNumber + i);
     result /= i;
   }
