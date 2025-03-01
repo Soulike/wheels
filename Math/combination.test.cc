6fb764b41164b30650912d17d9b75131bc3a63a1
Soulike <git_anonymous@foxmail.com>
2025-03-01 14:14:06 +0800

    test: update CMakeLists.txt to support testing

--- Math/combination.test.cc
+++ Math/combination.test.cc
@@ -30,8 +30,3 @@ TEST(CombinationTest, EdgeCases) {
   EXPECT_EQ(combination(0, 0), 1u);  // C(0,0) = 1
   EXPECT_EQ(combination(1, 0), 1u);  // C(1,0) = 1
 }
-
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
