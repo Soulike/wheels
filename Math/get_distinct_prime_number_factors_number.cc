d224e24d5a0e8e0c0d024401a44cc68e74419888
Soulike <git_anonymous@foxmail.com>
2025-03-29 11:34:57 +0800

    fix(Math): add comments to clarify prime factorization logic in GetTheNumberOfDistinctPrimeNumberFactors function

--- Math/get_distinct_prime_number_factors_number.cc
+++ Math/get_distinct_prime_number_factors_number.cc
@@ -3,8 +3,14 @@
 size_t GetTheNumberOfDistinctPrimeNumberFactors(const std::uintmax_t number) {
   size_t number_of_factors = 0;
   auto current_number = number;
-  for (std::uintmax_t i = 2; i <= current_number;i++) {
+  for (std::uintmax_t i = 2; i <= current_number; i++) {
     if (current_number % i == 0) {
+      // `i` must be a prime here.
+      // Because if `i` is not a prime, its factors should have been divided
+      // before.
+      // For example, if `i` is 6, then 2 and 3 must have been divided before
+      // when `i` was 2 and 3. So it is impossible for `current_number` to be
+      // still able to be divided by 6.
       number_of_factors++;
       while (current_number % i == 0) {
         current_number /= i;
